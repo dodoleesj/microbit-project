f0dd5a48ac9281d5b4592af73b1e22743dc8deaa
dodore <47679927+dodoleesj@users.noreply.github.com>
2020-12-05 16:31:02 +0900

    오 커밋도 되네?

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.West)
+    pins.servoWritePin(AnalogPin.P0, 0)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P0, 90)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.West)
+    pins.servoWritePin(AnalogPin.P0, 180)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P0, 90)
+})
+basic.showIcon(IconNames.Yes)
+pins.servoWritePin(AnalogPin.P0, 90)
